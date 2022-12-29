be59d4f1cc7b6588078be41e7e6612cfff8536d1
nataliasokolowska <n.martowska@gmail.com>
2022-12-29 08:17:13 +0000

    Added password field with validation. Language changed to English

--- src/validate.js
+++ src/validate.js
@@ -3,11 +3,17 @@ const isValidEmail = (email) => {
   return reg.test(String(email).toLowerCase());
 };
 
+const isValidPassword = (password) => {
+  const reg = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,16}$/;
+  return reg.test(String(password));
+};
+
 export const validateFields = (value, fieldName, returnValue) => {
   if (fieldName !== "") {
     if (fieldName === "name") {
       if (value.length < 3) {
-        returnValue.message = "Pole imię musi mieć przynajmniej 3 znaki";
+        returnValue.message =
+          "The name field should contain at least 3 characters";
         return;
       } else {
         returnValue.isValid = true;
@@ -16,10 +22,24 @@ export const validateFields = (value, fieldName, returnValue) => {
 
     if (fieldName === "email") {
       if (value === "") {
-        returnValue.message = "Wpisz email";
+        returnValue.message = "Add email";
         return;
       } else if (!isValidEmail(value)) {
-        returnValue.message = "Pole email jest błędne";
+        returnValue.message = "Invalid email address";
+        return;
+      } else {
+        returnValue.isValid = true;
+      }
+      return returnValue;
+    }
+
+    if (fieldName === "password") {
+      if (value === "") {
+        returnValue.message = "Add password";
+        return;
+      } else if (!isValidPassword(value)) {
+        returnValue.message =
+          "Password should contain min 8 max 16 characters, at least 1 symbol, upper and lower case letters and a number";
         return;
       } else {
         returnValue.isValid = true;
